84d3252ac80a99703533946ed86d9f623e095f09
SachinMhz <sachin.mhz.sm@gmail.com>
2020-08-02 17:03:55 +0545

    fix router with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter , Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import StoryPage from "./pages/StoryPage";
@@ -8,12 +8,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="Main">
-        <BrowserRouter>
+        <HashRouter >
           <div>
             <Route path="/" exact component={HomePage} />
             <Route path="/story/:id" component={StoryPage} />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   }
